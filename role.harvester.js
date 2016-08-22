2bb3d489d7654c5d3e61892b6daa1f2d480ca004
numinis <numinis@gmail.com>
2016-08-22 07:52:51 +0200

    update

--- role.harvester.js
+++ role.harvester.js
@@ -1,13 +1,12 @@
-var capacitySpawn = SPAWN_ENERGY_CAPACITY
 var roleUpgrader = require('role.upgrader');
-var roleBuilder = require ('role.builder');
+/*var roleBuilder = require ('role.builder');*/
 var roleHarvester = {
 
 
     run: function(creep) {
-        if (capacitySpawn === StructureSpawn.energyCapacity && creep.carry.RESOURCE_ENERGY == 50){
+        /*if (creep.room.SPAWN_ENERGY_CAPACITY === creep.room.energyCapacity && creep.carry.RESOURCE_ENERGY === 50){
          roleBuilder.run(creep)
-         }
+         }*/
 
         if(creep.carry.energy < creep.carryCapacity) {
             var sources = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
@@ -15,20 +14,24 @@ var roleHarvester = {
                 creep.moveTo(sources);
             }
         }
-        else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                        structure.energy < structure.energyCapacity;
-                }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
+
+        var targets = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                    structure.energy < structure.energyCapacity;
             }
+        })
+        if(targets.length > 0) {
+            if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0]);
+            }
+
         }
+        /*else if (creep.room.SPAWN_ENERGY_CAPACITY === StructureSpawn.energyCapacity && creep.carry.RESOURCE_ENERGY === 50){
+         roleBuilder.run(creep)
+         }*/
     }
-};
+}
+
 
 module.exports = roleHarvester;
